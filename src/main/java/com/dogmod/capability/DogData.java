2c6b088726cfb1c776c4405ac35bb44b558ce1de
legax-creator <my3reeder@gmail.com>
2026-08-18 11:35:16 +0300

    Create DogData.java

---
+++ src/main/java/com/dogmod/capability/DogData.java
@@ -0,0 +1,58 @@
+package com.dogmod.capability;
+
+import net.minecraft.nbt.NbtCompound;
+import java.util.UUID;
+
+public class DogData {
+    private UUID ownerUUID = null;
+    private boolean tamed = false;
+    private boolean sitting = false;
+    private boolean leashed = false;
+    private double leashAnchorX, leashAnchorY, leashAnchorZ;
+
+    public boolean isTamed() { return tamed; }
+    public void setTamed(boolean tamed) { this.tamed = tamed; }
+    public UUID getOwnerUUID() { return ownerUUID; }
+    public void setOwnerUUID(UUID ownerUUID) { this.ownerUUID = ownerUUID; }
+    public boolean isOwner(UUID uuid) { return ownerUUID != null && ownerUUID.equals(uuid); }
+    public boolean isSitting() { return sitting; }
+    public void setSitting(boolean sitting) { this.sitting = sitting; }
+    public boolean isLeashed() { return leashed; }
+    public void setLeashed(boolean leashed) { this.leashed = leashed; }
+    public double getLeashAnchorX() { return leashAnchorX; }
+    public double getLeashAnchorY() { return leashAnchorY; }
+    public double getLeashAnchorZ() { return leashAnchorZ; }
+
+    public void setLeashAnchor(double x, double y, double z) {
+        this.leashAnchorX = x; this.leashAnchorY = y; this.leashAnchorZ = z;
+        this.leashed = true;
+    }
+    public void clearLeash() {
+        this.leashed = false;
+        this.leashAnchorX = 0; this.leashAnchorY = 0; this.leashAnchorZ = 0;
+    }
+
+    public void writeToNbt(NbtCompound nbt) {
+        nbt.putBoolean("tamed", tamed);
+        nbt.putBoolean("sitting", sitting);
+        nbt.putBoolean("leashed", leashed);
+        if (ownerUUID != null) nbt.putUuid("ownerUUID", ownerUUID);
+        if (leashed) {
+            nbt.putDouble("leashX", leashAnchorX);
+            nbt.putDouble("leashY", leashAnchorY);
+            nbt.putDouble("leashZ", leashAnchorZ);
+        }
+    }
+
+    public void readFromNbt(NbtCompound nbt) {
+        this.tamed = nbt.getBoolean("tamed");
+        this.sitting = nbt.getBoolean("sitting");
+        this.leashed = nbt.getBoolean("leashed");
+        if (nbt.containsUuid("ownerUUID")) this.ownerUUID = nbt.getUuid("ownerUUID");
+        if (leashed) {
+            this.leashAnchorX = nbt.getDouble("leashX");
+            this.leashAnchorY = nbt.getDouble("leashY");
+            this.leashAnchorZ = nbt.getDouble("leashZ");
+        }
+    }
+}
