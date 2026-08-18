a0ba54902ba50ab806cea9c05afb7cf81584e11d
legax-creator <my3reeder@gmail.com>
2026-08-18 11:34:18 +0300

    Create DogOriginChecker.java

---
+++ src/main/java/com/dogmod/capability/DogOriginChecker.java
@@ -0,0 +1,29 @@
+package com.dogmod.capability;
+
+import io.github.apace100.origins.component.OriginComponent;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.origin.OriginLayer;
+import io.github.apace100.origins.origin.OriginLayers;
+import io.github.apace100.origins.registry.ModComponents;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class DogOriginChecker {
+    private static final String[] DOG_ORIGIN_IDS = {
+        "dogmod:dog", "moborigins:wolf", "origins:wolf", "extraorigins:wolf"
+    };
+
+    public static boolean isDogPlayer(ServerPlayerEntity player) {
+        try {
+            OriginComponent component = ModComponents.ORIGIN.get(player);
+            for (OriginLayer layer : OriginLayers.getLayers()) {
+                Origin origin = component.getOrigin(layer);
+                if (origin == null) continue;
+                String id = origin.getIdentifier().toString();
+                for (String dogId : DOG_ORIGIN_IDS) {
+                    if (id.equals(dogId)) return true;
+                }
+            }
+        } catch (Exception e) { return false; }
+        return false;
+    }
+}
