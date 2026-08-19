6572fdf441ade94c427bdde5b2d6ee97ba1fea38
legax-creator <my3reeder@gmail.com>
2026-08-19 13:50:50 +0300

    Update PlayerHitboxMixin.java

--- src/main/java/com/dogmod/mixin/PlayerHitboxMixin.java
+++ src/main/java/com/dogmod/mixin/PlayerHitboxMixin.java
@@ -1,5 +1,6 @@
 package com.dogmod.mixin;
 
+import com.dogmod.capability.DogDataManager;
 import com.dogmod.capability.DogOriginChecker;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityPose;
@@ -10,10 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-/**
- * Köpek oyuncunun hitbox'ını vanilla wolf boyutuna getirir.
- * Wolf: genişlik 0.6, yükseklik 0.85
- */
 @Mixin(PlayerEntity.class)
 public class PlayerHitboxMixin {
 
@@ -22,8 +19,6 @@ public class PlayerHitboxMixin {
         PlayerEntity player = (PlayerEntity)(Object)this;
         if (!(player instanceof ServerPlayerEntity sp)) return;
         if (!DogOriginChecker.isDogPlayer(sp)) return;
-
-        // Vanilla wolf boyutu: 0.6 x 0.85
         cir.setReturnValue(EntityDimensions.changing(0.6f, 0.85f));
     }
 }
