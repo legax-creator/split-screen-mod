265101b99457564fd599bde622fe0583d95670ce
legax-creator <my3reeder@gmail.com>
2026-08-19 13:46:28 +0300

    Update DogOriginChecker.java

--- src/main/java/com/dogmod/capability/DogOriginChecker.java
+++ src/main/java/com/dogmod/capability/DogOriginChecker.java
@@ -1,42 +1,20 @@
 package com.dogmod.capability;
 
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+/**
+ * Origins'e bağımlılık yok.
+ * Oyuncunun köpek olup olmadığını DogData üzerinden kontrol eder.
+ * /dog komutuyla köpek olunur, /dog leave ile çıkılır.
+ */
 public class DogOriginChecker {
-
-    private static final String[] DOG_ORIGIN_IDS = {
-        "dogmod:dog", "moborigins:wolf", "origins:wolf", "extraorigins:wolf"
-    };
 
     public static boolean isDogPlayer(ServerPlayerEntity player) {
         try {
-            NbtCompound nbt = new NbtCompound();
-            player.writeNbt(nbt);
-
-            // Origins modu verisini player NBT'sinde "origins:origin" altında saklar
-            if (!nbt.contains("ForgeCaps") && !nbt.contains("origins:origin")) {
-                // Fabric'te Origins, PlayerData NBT'sine yazar
-                // "origins" key'i altında layer -> originId şeklinde
-                if (nbt.contains("origins")) {
-                    NbtCompound originsNbt = nbt.getCompound("origins");
-                    for (String key : originsNbt.getKeys()) {
-                        String originId = originsNbt.getString(key);
-                        for (String dogId : DOG_ORIGIN_IDS) {
-                            if (originId.equals(dogId)) return true;
-                        }
-                    }
-                }
-            }
-
-            // Alternatif: persistentData içinde ara
-            if (nbt.contains("BukkitValues")) return false;
-
+            DogData data = DogDataManager.get(player);
+            return data.isDog();
         } catch (Exception e) {
             return false;
         }
-        return false;
     }
 }
