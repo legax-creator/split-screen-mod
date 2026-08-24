8925c2a4135693eeba26997e1b90eb9d7f4a2688
legax-creator <my3reeder@gmail.com>
2026-08-24 22:46:46 +0300

    Update DogDataManager.java

--- src/main/java/com/dogmod/capability/DogDataManager.java
+++ src/main/java/com/dogmod/capability/DogDataManager.java
@@ -1,15 +1,32 @@
 package com.dogmod.capability;
 
 import com.dogmod.DogMod;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerPlayerEvents;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class DogDataManager {
+
     private static final Map<UUID, DogData> DATA_MAP = new HashMap<>();
 
     public static void register() {
+        // Respawn olunca veriyi koru
+        ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) -> {
+            UUID uuid = newPlayer.getUuid();
+            if (DATA_MAP.containsKey(uuid)) {
+                DogMod.LOGGER.debug("Respawn: {} için dog data korundu", uuid);
+            }
+        });
+
+        // Veriyi NBT ile kaydet (sunucu kapatılınca)
+        net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            DogMod.LOGGER.info("Dog verileri kaydediliyor...");
+        });
+
         DogMod.LOGGER.info("DogDataManager hazır");
     }
 
@@ -23,5 +40,20 @@ public class DogDataManager {
 
     public static void remove(UUID uuid) {
         DATA_MAP.remove(uuid);
+    }
+
+    // Veriyi NBT'ye kaydet
+    public static NbtCompound save(UUID uuid) {
+        NbtCompound nbt = new NbtCompound();
+        DogData data = DATA_MAP.get(uuid);
+        if (data != null) data.writeToNbt(nbt);
+        return nbt;
+    }
+
+    // Veriyi NBT'den yükle
+    public static void load(UUID uuid, NbtCompound nbt) {
+        DogData data = new DogData();
+        data.readFromNbt(nbt);
+        DATA_MAP.put(uuid, data);
     }
 }
